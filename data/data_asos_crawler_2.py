9363841b13d7c18cc885f9eae8afc9b718c66b0b
acse-ww721 <114886100+acse-ww721@users.noreply.github.com>
2023-08-15 22:10:04 +0100

    fix the code logic

--- data/data_asos_crawler_2.py
+++ data/data_asos_crawler_2.py
@@ -7,20 +7,46 @@ import pandas as pd
 import datetime
 import time
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
+from tqdm import tqdm
 
 eu_member_codes = [
-    'AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 'FI', 'FR',
-    'DE', 'GR', 'HU', 'IE', 'IT', 'LV', 'LT', 'LU', 'MT', 'NL',
-    'PL', 'PT', 'RO', 'SK', 'SI', 'ES', 'SE', 'GB',
+    'AT',
+    'BE',
+    'BG',
+    'HR',
+    'CY',
+    'CZ',
+    'DK',
+    'EE',
+    'FI',
+    'FR',
+    'DE',
+    'GR',
+    'HU',
+    'IE',
+    'IT',
+    'LV',
+    'LT',
+    'LU',
+    'MT',
+    'NL',
+    'PL',
+    'PT',
+    'RO',
+    'SK',
+    'SI',
+    'ES',
+    'SE',
+    'GB',
 ]
 
 # Extract data time range
-startts =  datetime.datetime(2022,1,1)
-endts =  datetime.datetime(2023,8, 2)
+startts = datetime.datetime(2022, 1, 1)
+endts = datetime.datetime(2023, 8, 2)
 
 # Number of attempts to download data
 MAX_ATTEMPTS = 6
+
 
 def get_all_network():
     url = "https://mesonet.agron.iastate.edu/request/download.phtml?network=FR__ASOS"
@@ -30,13 +56,16 @@ def get_all_network():
     if select_element:
         option_elements = select_element.find_all("option")
 
-        option_values = [option["value"] for option in option_elements if option.get("value")]
+        option_values = [
+            option["value"] for option in option_elements if option.get("value")
+        ]
 
         print("Option values:")
         for value in option_values:
             print(value)
     else:
         print("Select element not found on the page.")
+
 
 def get_network_url(country_list):
     valid_urls = []
@@ -54,10 +83,13 @@ def get_network_url(country_list):
 
     return valid_urls
 
-def get_all_station_by_network(valid_urls):
+
+def get_all_station_by_network(country_list):
     # valid_urls = get_network_url()
-    for i in valid_urls:
-        url_station_geojson = f"{i}.geojson"
+    for i in country_list:
+        url_station_geojson = (
+            f"https://mesonet.agron.iastate.edu/geojson/network/{i}__ASOS.geojson"
+        )
 
         # Get GeoJSON data
         response = requests.get(url_station_geojson)
@@ -90,36 +122,44 @@ def get_all_station_by_network(valid_urls):
         df = pd.DataFrame(data)
     return df
 
-def get_data_url(df,startts,endts):
-    url_site ="https://mesonet.agron.iastate.edu/cgi-bin/request/asos.py?"
+
+def get_data_url(df, startts, endts):
+    url_site_list = []
+    id_list = []
+    url_site_header = "https://mesonet.agron.iastate.edu/cgi-bin/request/asos.py?"
+    url_site_tail = f"data=all&"  # add all data variables
+    url_site_tail += startts.strftime("year1=%Y&month1=%m&day1=%d&")  # add start date
+    url_site_tail += endts.strftime("year2=%Y&month2=%m&day2=%d&")  # add end date
+    url_site_tail += f"tz=Etc%2FUTC&format=onlycomma&latlon=yes&elev=yes&missing=null&trace=T&direct=no&report_type=3&report_type=4"  # add data format
     for id in df['ID']:
-        url_site+= f"station={id}&" # add all stations
-        url_site+= f"data=all&" # add all data variables
-        url_site+= startts.strftime("year1=%Y&month1=%m&day1=%d&") # add start date
-        url_site+=  endts.strftime("year2=%Y&month2=%m&day2=%d&")  # add end date
-        url_site+= f"tz=Etc%2FUTC&format=onlycomma&latlon=yes&elev=yes&missing=null&trace=T&direct=no&report_type=3&report_type=4"    # add data format
+        url_site = f"{url_site_header}station={id}&{url_site_tail}"  # add all stations
+        url_site_list.append(url_site)
+        id_list.append(id)
 
-    return url_site
+    return url_site_list, id_list  # return station list
 
-def download_data(url_data):
+
+def download_data(url_site):
     attempt = 0
     while attempt < MAX_ATTEMPTS:
         try:
-            response = requests.get(url_data, timeout=300)
+            response = requests.get(url_site, timeout=300)
             data = response.text
             if data is not None and not data.startswith("ERROR"):
                 return data
         except Exception as e:
-            print(f"download_data({url_data}) failed with {e}")
+            print(f"download_data({url_site}) failed with {e}")
             time.sleep(5)
         attempt += 1
 
     print("Exhausted attempts to download, returning empty data")
     return ""
 
-def save_data(url_site,country,startts,endts):
+
+def save_data(url_site, country, station, startts, endts):
     data = download_data(url_site)
-    output_filename = f"{country}_{startts:%Y%m%d%H%M}_{endts:%Y%m%d%H%M}.csv"
+    # output_filename = f"{country}_{startts:%Y%m%d%H%M}_{endts:%Y%m%d%H%M}.csv"
+    output_filename = f"{country}_{station}_{startts:%Y%m%d}_{endts:%Y%m%d}.csv"
     # Split the data into lines
     lines = data.split("\n")
 
@@ -139,21 +179,30 @@ def save_data(url_site,country,startts,endts):
     df = pd.DataFrame(data_rows, columns=column_names)
 
     # Save the DataFrame to a CSV file
-    df.to_csv( output_filename , index=False, encoding="utf-8")
+    df.to_csv(output_filename, index=False, encoding="utf-8")
+    print(f'{output_filename} done!')
+
 
 # UK example
-country = ["GB",]
-start_date =  datetime.datetime(2022,1,1)
-endt_date =  datetime.datetime(2023,8, 2)
+country = [
+    "GB",
+]
+start_date = datetime.datetime(2022, 1, 1)
+end_date = datetime.datetime(2023, 8, 2)
 
-valid_url = get_network_url(country)
-gb_df = get_all_station_by_network(valid_url)
-url_site = get_data_url(gb_df,start_date,endt_date)
-save_data(url_site,"GB",start_date,endt_date)
+gb_df = get_all_station_by_network(country)
+url_site_list, id_list = get_data_url(gb_df, start_date, end_date)
+for url_site, station_id in tqdm(zip(url_site_list, id_list)):
+    start_time = time.time()  # Record start time
+    save_data(url_site, "GB", station_id, start_date, end_date)
+    end_time = time.time()  # Record end time
+    download_time = end_time - start_time
+    print(f'Download time: {download_time:.3f} s')
 
 
-
-
-
-
-
+# test url:
+"""
+https://mesonet.agron.iastate.edu/cgi-bin/request/asos.py?
+station=EGAA&data=all&year1=2023&month1=8&day1=14&year2=2023&month2=8&day2=15&tz=Etc%2FUTC&format=onlycomma&latlon=yes&elev=yes&missing=M&trace=T
+&direct=no&report_type=3&report_type=4
+"""
