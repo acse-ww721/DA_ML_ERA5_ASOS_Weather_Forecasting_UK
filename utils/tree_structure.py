f766fdccecf45750615d9f7be16fa91ba6497739
acse-ww721 <114886100+acse-ww721@users.noreply.github.com>
2023-08-16 17:50:23 +0100

    update tree structure

--- utils/tree_structure.py
+++ utils/tree_structure.py
@@ -1,13 +1,18 @@
 import os
-from folder_utils import get_current_directory
-def generate_tree(directory, indent="", output_file=None):
+
+
+def generate_tree(directory, indent="", output_file=None, skip_folders=None):
     line = indent + "|-- " + os.path.basename(directory) + "\n"
     if os.path.isdir(directory):
         items = sorted(os.listdir(directory))
         for item in items:
             item_path = os.path.join(directory, item)
+            if skip_folders and os.path.basename(item_path) in skip_folders:
+                continue  # Skip specified folders
             if os.path.isdir(item_path):
-                line += generate_tree(item_path, indent + "    ")
+                line += generate_tree(
+                    item_path, indent + "    ", skip_folders=skip_folders
+                )
             else:
                 line += indent + "    |-- " + item + "\n"
     if output_file:
@@ -23,5 +28,19 @@ if __name__ == "__main__":
     with open(output_file_path, "w") as f:
         f.write("File Tree Structure:\n")
 
-    generate_tree(root_directory, output_file=output_file_path)
+    # List of folders to skip
+    folders_to_skip = [
+        ".git",
+        ".DS_Store",
+        "logs",
+        "objects",
+        "refs",
+        ".idea",
+        ".ipynb_checkpoints",
+        "script_",
+    ]
+
+    generate_tree(
+        root_directory, output_file=output_file_path, skip_folders=folders_to_skip
+    )
     print("File tree structure generated and saved to 'tree_structure.txt'")
