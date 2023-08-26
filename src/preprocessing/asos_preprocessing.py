65bb48a9a593da16e10f7ab553fbf41ba2d4a899
acse-ww721 <114886100+acse-ww721@users.noreply.github.com>
2023-08-26 19:52:54 +0100

    asos v2

--- src/preprocessing/asos_preprocessing.py
+++ src/preprocessing/asos_preprocessing.py
@@ -1,9 +1,36 @@
 import os
 import pandas as pd
-from utils import folder_utils, time_utils
 from tqdm import tqdm
+from datetime import datetime, timedelta
+from utils import folder_utils
 
-"""OLD VERSION"""
+"""V2"""
+
+
+def get_csv_list(country, data_folder, data_category, output_folder):
+    # Specify the folder path
+    input_folder_path = folder_utils.find_folder(
+        country, data_folder, data_category, output_folder
+    )
+    # Initialize a dataframe to store all the data from the same country
+    raw_df = pd.DataFrame()
+    # Get the filenames of all CSV files under the folder except the station network file
+    csv_files = [
+        f
+        for f in os.listdir(input_folder_path)
+        if f.endswith(".csv") and "asos_station_network" not in f
+    ]
+    # Add path
+    csv_file_paths = []  # Initialize a list to store the path of all csv files
+    station_list = []  # Initialize a list to store the station name of all csv files
+    for csv_file in tqdm(csv_files):
+        csv_path = os.path.join(input_folder_path, csv_file)
+        # Extract station from the filename
+        station = csv_file.split("_")[1]
+        csv_file_paths.append(csv_path)
+        station_list.append(station)
+
+    return csv_file_paths, station_list
 
 
 def extract_data_to_df(country, data_folder, data_category, output_folder):
@@ -28,84 +55,97 @@ def extract_data_to_df(country, data_folder, data_category, output_folder):
     return raw_df
 
 
-def process_asos_rawdata(df, start_date, end_date):
+def time_rearrange(df):
+    """
+    For hourly initial times (such as 01:00:00, 02:00:00, etc.), hold.
+    For the 20th minute of every hour (such as 01:20:00, 02:20:00, etc.),
+    check whether the previous full point exists, and delete the 20-minute data if it exists,
+    otherwise change to the previous full point.
+    For the 50th minute of every hour (such as 01:50:00, 02:50:00, etc.),
+    check whether the next full point exists, and if it exists, delete the 50-minute data,
+    otherwise change to the next full point
+
+    df is a DataFrame with a column named 'valid' containing time strings.
+    Return the processed DataFrame.
+    """
+    # Create an assistant volume and convert time strings to datetime objects
+    df["valid_datetime"] = pd.to_datetime(df["valid"], format="%d/%m/%Y %H:%M:%S")
+
+    # Delete the data before 1979-01-01
+    cutoff_date = datetime(1979, 1, 1)
+
+    # Check if the earliest date is after the cutoff date
+    earliest_date = df["valid_datetime"].min()
+    if earliest_date >= cutoff_date:
+        raise ValueError(
+            f"The start date :  ({earliest_date.strftime('%d/%m/%Y')}) is after 19790101, terminate cutoff."
+        )
+
+    df = df[df["valid_datetime"] >= cutoff_date]
+
+    # Create a list to store the indexes of rows to be deleted
+    to_delete = []
+
+    for index, row in df.iterrows():
+        current_time = row["valid_datetime"]
+
+        # If the minute is 20 minutes
+        if current_time.minute == 20:
+            prev_hour_time = current_time.replace(minute=0, second=0)
+            if (
+                prev_hour_time in df["valid_datetime"].values
+            ):  # if the previous full point exists
+                to_delete.append(index)  # record the current index for later deletion
+            else:
+                df.at[
+                    index, "valid_datetime"
+                ] = prev_hour_time  # otherwise change to the previous full point
+
+        # if the minute is 50 minutes
+        elif current_time.minute == 50:
+            next_hour_time = (current_time + timedelta(hours=1)).replace(
+                minute=0, second=0
+            )
+            if (
+                next_hour_time in df["valid_datetime"].values
+            ):  # if the next full point exists
+                to_delete.append(index)  # record the current index for later deletion
+            else:
+                df.at[
+                    index, "valid_datetime"
+                ] = next_hour_time  # otherwise change to the next full point
+
+    # delete the rows that need to be deleted
+    df.drop(to_delete, inplace=True)
+
+    # Delete the original time column
+    df.drop(columns=["valid"], inplace=True)
+
+    # Rename the new time column and  it save as iso 8601 format as datetime64[ns] automatically
+    df.rename(columns={"valid_datetime": "time"}, inplace=True)
+    # df["time"] = df["time"].dt.strftime("%Y-%m-%dT%H:%M:%S.%f")
+
+    return df
+
+
+def process_asos_rawdata(raw_df):
     """
     Unified variable unit based on era5
     station: three or four character site identifier
-    lat: Latitude of the observation station
-    lon: Longitude of the observation station
-    tmpf: Air Temperature in Kelvin, typically @ 2 meters
-    dwpf: Dew Point Temperature in Kelvin, typically @ 2 meters
-    relh: Relative Humidity in %
-    drct: Wind Direction in degrees from *true* north
-    sknt: Wind Speed in m/s
-    p01i: One hour precipitation for the period from the observation time to the time of the previous hourly precipitation reset.
-          This varies slightly by site. Values are in inches. This value may or may not contain frozen precipitation melted by
-          some device on the sensor or estimated by some other means. Unfortunately, we do not know of
-          an authoritative database denoting which station has which sensor.
-    alti: Pressure altimeter in meters
-    mslp: Sea Level Pressure in Pa
-    gust: Wind Gust in m/s
-    feel: Apparent Temperature (Wind Chill or Heat Index) in Kelvin
+    valid: observation time in UTC
+    tmpc: Air Temperature in Celsius, typically @ 2 meters
+
     """
 
-    # Split "valid" column into "date" and "time" columns
-    df["date"] = (
-        df["valid"].str.split(" ", expand=True)[0].str.replace("-", "").astype(int)
-    )
-    df["time"] = df["valid"].str.split(" ", expand=True)[1]
+    # time preprocessing
+    processed_df = time_rearrange(raw_df)
 
-    # Convert Fahrenheit to Kelvin for "tmpf", "feel" and "dwpf" columns
-    df["tmpf"] = (df["tmpf"] - 32) * 5 / 9 + 273.15
-    df["tmpf"] = df["tmpf"].round(1)
+    # Convert Celsius to Kelvin for "tmpc" and rename the column
+    processed_df.rename(columns={"tmpc": "t2m"}, inplace=True)
+    processed_df["t2m"] = processed_df["t2m"] + 273.15
+    processed_df["t2m"] = processed_df["t2m"].round(1)
 
-    df["dwpf"] = (df["dwpf"] - 32) * 5 / 9 + 273.15
-    df["dwpf"] = df["dwpf"].round(1)
-
-    # Convert Fahrenheit to Kelvin for "feel" column
-    df["feel"] = (df["feel"] - 32) * 5 / 9 + 273.15
-    df["feel"] = df["feel"].round(1)
-
-    # Convert knots to m/s for "sknt" and "gust" columns
-    df["sknt"] = df["sknt"] * 0.514444
-    df["gust"] = df["gust"] * 0.514444
-
-    # Convert inches to meters for "p01i" and "alti" columns
-    df["p01i"] = pd.to_numeric(df["p01i"], errors="coerce") * 0.0254
-
-    df["alti"] = df["alti"] * 0.0254
-
-    # Convert millibar to Pa for "mslp" column
-    df["mslp"] = df["mslp"] * 100
-
-    # Drop columns
-    columns_to_drop = [
-        "lon",
-        "lat",
-        "elevation",
-        "valid",
-        "skyc1",
-        "skyc2",
-        "skyc3",
-        "skyc4",
-        "skyl1",
-        "skyl2",
-        "skyl3",
-        "skyl4",
-        "wxcodes",
-        "ice_accretion_1hr",
-        "ice_accretion_3hr",
-        "ice_accretion_6hr",
-        "peak_wind_gust",
-        "peak_wind_drct",
-        "peak_wind_time",
-        "metar",
-        "snowdepth",
-    ]
-    df.drop(columns=columns_to_drop, inplace=True, errors="ignore")
-    # TODO interpolation for the time data from half a hour to a hour
-
-    return df
+    return processed_df
 
 
 def merge_csv_station(country, data_folder, data_category, output_folder):
@@ -140,12 +180,12 @@ def merge_csv_station(country, data_folder, data_category, output_folder):
 
 
 def save_asos_processed_data(
-    processed_df, country, data_folder, data_category, output_folder
+    processed_df, station, country, data_folder, data_category, output_folder
 ):
     output_directory = folder_utils.create_folder(
         country, data_folder, data_category, output_folder
     )
-    output_filename = f"{country}_ASOS_processed_data.csv"
+    output_filename = f"{country}_ASOS_{station}_processed_data.csv"
     output_filepath = os.path.join(output_directory, output_filename)
     processed_df.to_csv(output_filepath, index=False, encoding="utf-8")
     print(f"{output_filename} done!")
@@ -158,12 +198,15 @@ data_folder = "data"
 data_read_category = "raw_data"
 data_save_category = "processed_data"
 output_folder = "ASOS_DATA"
-start_date = pd.Timestamp("2022-08-01")
-end_date = pd.Timestamp("2023-08-01")
 
-raw_df = extract_data_to_df(country, data_folder, data_read_category, output_folder)
-processed_df = process_asos_rawdata(raw_df)
-processed_df = time_utils.time_select(processed_df, "date", start_date, end_date)
-save_asos_processed_data(
-    processed_df, country, data_folder, data_save_category, output_folder
+csv_list, station_list = get_csv_list(
+    country, data_folder, data_read_category, output_folder
 )
+
+for csv_path, station in tqdm(zip(csv_list, station_list)):
+    print(csv_path)
+    raw_df = pd.read_csv(csv_path)
+    processed_df = process_asos_rawdata(raw_df)
+    save_asos_processed_data(
+        processed_df, station, country, data_folder, data_save_category, output_folder
+    )
