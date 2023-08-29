931b3a88d73b81570868f91d5ad6734498bd254a
acse-ww721 <114886100+acse-ww721@users.noreply.github.com>
2023-08-29 17:19:34 +0100

    change from numpy to pytorch

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -1,9 +1,21 @@
 import numpy as np
+import torch
+
+
+def get_initial_weights_torch(output_size):
+    b = torch.zeros(2, 3, dtype=torch.float32)
+    # Identity transformation: set the main diagonal to 1
+    b[0, 0] = 1
+    b[1, 1] = 1
+
+    # Initialize the weights to zero
+    W = torch.zeros(output_size, 6, dtype=torch.float32)
+
+    return W, b.view(-1)
+
 
 # The function implementation below is a modification based on DDWP-DA's GitHub code
 # Original code link: https://github.com/ashesh6810/DDWP-DA/blob/master/utils.py
-
-
 def get_initial_weights(output_size):
     b = np.zeros((2, 3), dtype="float32")
     # Identity transformation: set the main diagonal to 1
