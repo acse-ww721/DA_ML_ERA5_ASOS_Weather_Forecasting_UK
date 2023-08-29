11ff46c16124c8e0c0b22d9422ce7efad2e5f3f1
acse-ww721 <114886100+acse-ww721@users.noreply.github.com>
2023-08-29 17:02:05 +0100

    model_v2

--- src/models/Unet_STN_x.py
+++ src/models/Unet_STN_x.py
@@ -1,58 +1,116 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from utils import model_utils
+from bilinear_interpolation import BilinearInterpolation
 
 
 class STN(nn.Module):
     def __init__(self, input_shape=(1, 32, 64), sampling_size=(8, 16), num_classes=10):
         super(STN, self).__init__()
+        self.input_shape = input_shape
+        self.sampling_size = sampling_size
+        self.num_classes = num_classes
 
-        # Localisation network
-        self.localization = nn.Sequential(
-            nn.Conv2d(input_shape[0], 32, kernel_size=5, padding=2),
-            nn.ReLU(True),
+        # Note: PyTorch uses B, C, H, W ordering while TensorFlow uses B, H, W, C
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(self.input_shape[0], 32, kernel_size=5, padding=2),
+            nn.ReLU(),
             nn.Conv2d(32, 32, kernel_size=5, padding=2),
-            nn.ReLU(True),
-            nn.MaxPool2d(2),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+        )
+
+        self.conv2 = nn.Sequential(
             nn.Conv2d(32, 32, kernel_size=5, padding=2),
-            nn.ReLU(True),
+            nn.ReLU(),
             nn.Conv2d(32, 32, kernel_size=5, padding=2),
-            nn.ReLU(True),
-            nn.MaxPool2d(2),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+        )
+
+        self.conv3 = nn.Sequential(
             nn.Conv2d(32, 32, kernel_size=5, padding=2),
-            nn.ReLU(True),
+            nn.ReLU(),
             nn.Conv2d(32, 32, kernel_size=5, padding=2),
-            nn.ReLU(True),
-            # Fully connected layer
+            nn.ReLU(),
+        )
+
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(32, 32, kernel_size=5, padding=2),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, kernel_size=5, padding=2),
+            nn.ReLU(),
+        )
+
+        # Assuming you'll write the BilinearInterpolation layer in PyTorch
+        # For now, I've commented out the related lines.
+        # self.bilinear_interpolation = BilinearInterpolation(sampling_size)
+
+        self.locnet = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(32 * 8 * 16, 500),
-            nn.ReLU(True),
+            nn.Linear(32 * self.sampling_size[0] * self.sampling_size[1], 500),
+            nn.ReLU(),
             nn.Linear(500, 200),
-            nn.ReLU(True),
+            nn.ReLU(),
             nn.Linear(200, 100),
-            nn.ReLU(True),
+            nn.ReLU(),
             nn.Linear(100, 50),
-            nn.ReLU(True),
-            nn.Linear(50, 6),
+            nn.ReLU(),
+            # Initialize weights here if necessary
+            nn.Linear(50, 6, model_utils.get_initial_weights(6)),
         )
 
-        # Initialize the weights/bias with identity transformation
-        self.localization[-1].weight.data.zero_()
-        self.localization[-1].bias.data.copy_(
-            torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float)
+        self.upconv1 = nn.Sequential(
+            nn.Conv2d(32, 32, kernel_size=2, padding=1), nn.ReLU()  # up6
         )
 
-        # Rest of the network
-        # ... to be added ...
+        self.conv6 = nn.Sequential(
+            nn.Conv2d(32, 32, kernel_size=5, padding=2),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, kernel_size=5, padding=2),
+            nn.ReLU(),
+        )
+
+        self.upconv2 = nn.Sequential(
+            nn.Conv2d(32, 32, kernel_size=2, padding=1), nn.ReLU()  # up7
+        )
+
+        self.conv7 = nn.Sequential(
+            nn.Conv2d(32, 32, kernel_size=5, padding=2),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, kernel_size=5, padding=2),
+            nn.ReLU(),
+        )
+
+        self.conv10 = nn.Conv2d(32, 1, kernel_size=5, padding=2)
 
     def forward(self, x):
-        theta = self.localization(x)
-        theta = theta.view(-1, 2, 3)
-        grid = F.affine_grid(theta, x.size())
-        x = F.grid_sample(x, grid)
-        return x  # Continue the network operations...
+        x1 = self.conv1(x)
+        x2 = self.conv2(x1)
+        x3 = self.conv3(x2)
+        x5 = self.conv5(x3)
+
+        # Apply locnet to flattened x5
+        theta = self.locnet(x5)
+
+        # You need to apply bilinear interpolation here with `theta`
+        # For now, I'll keep it as it is.
+        # x_transformed = self.bilinear_interpolation(x, theta)
+
+        up6 = F.interpolate(x, scale_factor=2, mode="nearest")
+        up6 = self.upconv1(up6)
+        up6 = torch.cat([up6, x2], 1)
+        x6 = self.conv6(up6)
+
+        up7 = F.interpolate(x6, scale_factor=2, mode="nearest")
+        up7 = self.upconv2(up7)
+        up7 = torch.cat([up7, x1], 1)
+        x7 = self.conv7(up7)
+
+        x10 = self.conv10(x7)
+        return x10
 
 
-# Instantiate and print the network to verify
-stn = STN()
-print(stn)
+model = STN()
+print(model)
