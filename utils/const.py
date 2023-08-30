2676b230810f36df648eaa181f96431aa412996b
acse_ww721 <ww721@ic.ac.uk>
2023-08-31 00:39:55 +0100

    clean up

--- utils/const.py
+++ utils/const.py
@@ -1,7 +1,7 @@
 
 # The constant is calculated by The data from 1979-2020 (Training set)
 # The calculation process can be seen in src\preprocessing\era5_preprocessing.py
-# The running process can be seen in src\preprocessing\test_data_era5_pre_v2.ipynb
+# The running process can be seen in src/models/test_Unet_STN_lead12.ipynb
 
 MEAN_T2M_1979_2020 = 273.77817
 STD_T2M_1979_2020 = 2.5819736
