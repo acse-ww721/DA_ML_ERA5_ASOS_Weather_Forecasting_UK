105870b7a1c57d4515a041f2d3cca171e4a2793b
acse-ww721 <114886100+acse-ww721@users.noreply.github.com>
2023-09-02 16:40:09 +0100

    assimilation

--- src/assimilation/assimilation.py
+++ src/assimilation/assimilation.py
@@ -1,128 +1,166 @@
-import os
 import numpy as np
-from collections import defaultdict
-from enum import Enum
-from openda.utils import Matrix, Results
-from openda.utils.io import FileBasedModelState, KalmanGainStorage
-from openda.interfaces import (
-    IVector,
-    IMatrix,
-    ITime,
-    IResultWriter,
-    IObservationDescriptions,
-    IStochObserver,
-    IStochModelInstance,
+import netCDF4 as nc
+import scipy.io as sio
+from src.models.model_unet_stn_1x import stn
+
+### This .mat file has been generated from the ERA5 lat-lon data ####
+file = sio.loadmat("ERA_grid.mat")
+lat = file["lat"]
+lon = file["lon"]
+
+########## This is the testing set #######
+
+fileList_test = []
+fileList_test.append("geopotential_500hPa_2018_5.625deg.nc")
+
+
+########### Ensure same normalization coefficient as trainig #######
+file = nc.Dataset("ERA_Z500_1hour.nc")
+Z500 = np.asarray(file["input"])
+M = np.mean(Z500.flatten())
+sdev = np.std(Z500.flatten())
+
+
+####### True data (noise free) for twin DA experiments ##########
+F = nc.Dataset(fileList_test[0])
+Z = np.asarray(F["z"])
+TRUTH = Z
+
+### Meshgrid for plotting ###
+[qx, qy] = np.meshgrid(lon, lat)
+
+
+##### Add noise to the truth to mimic observations####
+#### Value 1 is 1*\sigma_Z. See more in paper #####
+Z_rs = np.reshape(Z, [np.size(Z, 0), int(np.size(Z, 1) * np.size(Z, 2))])
+TRUTH = Z_rs
+Z_rs = (Z_rs - M) / sdev
+TRUTH = (TRUTH - M) / sdev
+noise = 1
+for k in range(1, np.size(Z_rs, 0)):
+    Z_rs[k - 1, :] = Z_rs[k - 1, :] + np.random.normal(0, noise, 2048)
+
+
+print("length of initial condition", len(Z_rs[0, :]))
+
+#### SPNEKF implementation following Tyrus Berry's implementation ######
+
+
+def ENKF(x, n, P, Q, R, obs, model, u_ensemble):
+    obs = np.reshape(obs, [n, 1])
+    x = np.reshape(x, [n, 1])
+    [U, S, V] = np.linalg.svd(P)
+    D = np.zeros([n, n])
+    np.fill_diagonal(D, S)
+    sqrtP = np.dot(np.dot(U, np.sqrt(D)), U)
+    ens = np.zeros([n, 2 * n])
+    ens[:, 0:n] = np.tile(x, (1, n)) + sqrtP
+    ens[:, n:] = np.tile(x, (1, n)) - sqrtP
+    ## forecasting step,dummy model
+
+    for k in range(0, np.size(ens, 1)):
+        u = model.predict(np.reshape(ens[:, k], [1, 32, 64, 1]))
+
+        u_ensemble[:, k] = np.reshape(u, (32 * 64,))
+
+    ############################
+    x_prior = np.reshape(np.mean(u_ensemble, 1), [n, 1])
+    print("shape pf x_prior", np.shape(x_prior))
+    print("shape pf obs", np.shape(obs))
+    cf_ens = ens - np.tile(x_prior, (1, 2 * n))
+    P_prior = np.dot(cf_ens, np.transpose(cf_ens)) / (2 * n - 1) + Q
+    h_ens = ens
+    y_prior = np.reshape(np.mean(h_ens, 1), [n, 1])
+    ch_ens = h_ens - np.tile(y_prior, (1, 2 * n))
+    print("shape pf y_prior", np.shape(y_prior))
+    P_y = np.dot(ch_ens, np.transpose(ch_ens)) / (2 * n - 1) + R
+    P_xy = np.dot(cf_ens, np.transpose(ch_ens)) / (2 * n - 1)
+    K = np.dot(P_xy, np.linalg.inv(P_y))
+    P = P_prior - np.dot(np.dot(K, P_y), np.transpose(K))
+    x = x_prior + np.dot(K, (obs - y_prior))
+
+    return x, P
+
+
+import tensorflow
+import keras.backend as K
+
+# from data_manager import ClutteredMNIST
+# from visualizer import plot_mnist_sample
+# from visualizer import print_evaluation
+# from visualizer import plot_mnist_grid
+import netCDF4
+import numpy as np
+from keras.layers import (
+    Input,
+    Convolution2D,
+    Convolution1D,
+    MaxPooling2D,
+    Dense,
+    Dropout,
+    Flatten,
+    concatenate,
+    Activation,
+    Reshape,
+    UpSampling2D,
+    ZeroPadding2D,
+)
+import keras
+from keras.callbacks import History
+
+history = History()
+
+
+model = stn()
+model.load_weights("best_weights_lead1.h5")
+### This code performs DA at every 24 hrs with a model that is forecasting every hour. So lead will always be 1 ######
+
+
+###### Start Data Assimilation Process #########################################
+
+time = 1200
+n = int(32 * 64)
+P = np.eye(n, n)
+
+Q = 0.03 * np.eye(n, n)
+
+R = 0.0001
+
+u_ensemble = np.zeros([32 * 64, 2 * 32 * 64])
+
+pred = np.zeros([time, 32, 64, 1])
+
+
+dt = 24
+count = 0
+for t in range(0, time, dt):
+    for kk in range(0, dt - 1):
+        if kk == 0:
+            u = Z_rs[t + kk, :].reshape([1, 32, 64, 1])
+            u = model.predict(u.reshape([1, 32, 64, 1]))
+        else:
+            u = model.predict(u)
+
+        pred[count, :, :, 0] = np.reshape(u, [32, 64])
+        count = count + 1
+    x = u
+    x, P = ENKF(x, 2048, P, Q, R, Z_rs[t + dt, :], model, u_ensemble)
+
+    print("output shape of ENKF", np.shape(x))
+
+    pred[count, :, :, 0] = np.reshape(x, [32, 64])
+    count = count + 1
+
+
+sio.savemat(
+    "DA_every24HR_lead1200_everytime_noise_" + str(noise) + ".mat",
+    dict(
+        [
+            ("prediction", pred),
+            ("truth", np.reshape(TRUTH, [np.size(Z_rs, 0), 32, 64, 1])),
+            ("noisy_obs", np.reshape(Z_rs, [np.size(Z_rs, 0), 32, 64, 1])),
+        ]
+    ),
 )
 
-
-class LocalizationMethodType(Enum):
-    hamill = 1
-    autoZhang = 2
-
-
-class EnKF:
-    def __init__(self):
-        self.gainVectors = defaultdict(IVector)
-        self.obsId = {}
-        self.obsTimeOffset = {}
-        self.smoothedGainMatrix = None
-        # Add other attributes here
-
-    def analysis(
-        self,
-        obs: IStochObserver,
-        obsValues: IVector,
-        predictions: IVector,
-        mainModel: IStochModelInstance,
-        analysisTime: ITime,
-    ):
-        self.init_all_timers()
-
-        with self.timer_total:
-            ensemble_vectors_forecast = self.get_ensemble_vectors_state(False)
-            ensemble_predictions_forecast = self.get_ensemble_vectors_prediction(
-                obs.get_observation_descriptions(), False
-            )
-
-            self.set_current_state(ensemble_vectors_forecast.mean.clone())
-
-            Kvecs = self.compute_gain_matrix(
-                obs, ensemble_predictions_forecast, ensemble_vectors_forecast
-            )
-
-            self.apply_localization_to_gain(
-                obs, Kvecs, ensemble_predictions_forecast, ensemble_vectors_forecast
-            )
-
-            if self.time_regularisation_per_day > 0.0:
-                if self.smoothed_gain_matrix is None:
-                    self.smoothed_gain_matrix = SmoothedGainMatrix(
-                        self.time_regularisation_per_day
-                    )
-                self.smoothed_gain_matrix.smooth_gain(
-                    obs, Kvecs, self.time_regularisation_per_day, analysisTime
-                )
-
-            self.store_gain_matrix(obs, analysisTime, Kvecs)
-
-            self.update_model_with_gain(
-                obs, ensemble_predictions_forecast, ensemble_vectors_forecast, Kvecs
-            )
-
-            for j in range(len(Kvecs)):
-                Kvecs[j].free()
-            ensemble_vectors_forecast.free()
-            ensemble_predictions_forecast.free()
-
-            ensemble_vectors_analysis = self.get_ensemble_vectors_state(True)
-            ensemble_predictions_analysis = self.get_ensemble_vectors_prediction(
-                obs.get_observation_descriptions(), True
-            )
-
-            self.update_main_model(ensemble_vectors_analysis)
-
-            ensemble_vectors_analysis.free()
-            ensemble_predictions_analysis.free()
-
-    def init_all_timers(self):
-        if self.timer_total is None:
-            self.timer_total = OdaTiming("analysis")
-            self.timer_synchronize = OdaTiming("Synchronize")
-            # Add other timers here
-            self.timer_total.add_sub_timer(self.timer_synchronize)
-            # Add other sub-timers here
-
-    # Add other methods here
-
-
-class SmoothedGainMatrix:
-    def __init__(self, time_regularisation_per_day):
-        self.last_gain_matrix_hash_map = {}
-        self.obs_ids = {}
-        self.last_obs_time_offsets = {}
-        self.previous_analysis_time = None
-        self.time_regularisation_per_day = time_regularisation_per_day
-
-    def smooth_gain(self, obs, K, time_regularisation_per_day, analysis_time):
-        n_cols = len(K)
-        obs_utils = ObserverUtils(obs)
-        obs_ids = obs_utils.get_obs_ids()
-        obs_time_offsets = obs_utils.get_obs_time_offsets(analysis_time.get_mjd())
-
-        if not self.last_gain_matrix_hash_map:
-            for i in range(obs.get_count()):
-                gain_vector_id = (
-                    f"{obs_ids[i]}:{round(obs_time_offsets[i] * 24.0 * 3600.0)}"
-                )
-                self.last_gain_matrix_hash_map[gain_vector_id] = K[i].clone()
-                self.obs_ids[gain_vector_id] = obs_ids[i]
-                self.last_obs_time_offsets[gain_vector_id] = obs_time_offsets[i]
-        else:
-            # TODO: Implement the rest of the smoothing logic here
-            pass
-
-
-# Instantiate EnKF and call the analysis method
-enkf = EnKF()
-# Initialize other variables and objects as needed
-enkf.analysis(obs, obs_values, predictions, main_model, analysis_time)
+print("Done writing file")
--- src/models/model_unet_stn_1x.py
+++ src/models/model_unet_stn_1x.py
@@ -1,43 +1,82 @@
+import tensorflow
+import keras.backend as K
+
+# from data_manager import ClutteredMNIST
+# from visualizer import plot_mnist_sample
+# from visualizer import print_evaluation
+# from visualizer import plot_mnist_grid
+import netCDF4
+import numpy as np
+from keras.layers import (
+    Input,
+    Convolution2D,
+    Convolution1D,
+    MaxPooling2D,
+    Dense,
+    Dropout,
+    Flatten,
+    concatenate,
+    Activation,
+    Reshape,
+    UpSampling2D,
+    ZeroPadding2D,
+)
+import keras
+from keras.callbacks import History
+
+
 def stn(input_shape=(32, 64, 1), sampling_size=(8, 16), num_classes=10):
     inputs = Input(shape=input_shape)
-    conv1 = Convolution2D(32, 5, 5, activation='relu', border_mode='same')(inputs)
-    conv1 = Convolution2D(32, 5, 5, activation='relu', border_mode='same')(conv1)
+    conv1 = Convolution2D(32, 5, 5, activation="relu", border_mode="same")(inputs)
+    conv1 = Convolution2D(32, 5, 5, activation="relu", border_mode="same")(conv1)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
-    conv2 = Convolution2D(32, 5, 5, activation='relu', border_mode='same')(pool1)
-    conv2 = Convolution2D(32, 5, 5, activation='relu', border_mode='same')(conv2)
+    conv2 = Convolution2D(32, 5, 5, activation="relu", border_mode="same")(pool1)
+    conv2 = Convolution2D(32, 5, 5, activation="relu", border_mode="same")(conv2)
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
-    conv3 = Convolution2D(32, 5, 5, activation='relu', border_mode='same')(pool2)
+    conv3 = Convolution2D(32, 5, 5, activation="relu", border_mode="same")(pool2)
     # conv3 = Convolution2D(32, 5, 5, activation='relu', border_mode='same')(conv3)
 
-    conv5 = Convolution2D(32, 5, 5, activation='relu', border_mode='same')(conv3)
+    conv5 = Convolution2D(32, 5, 5, activation="relu", border_mode="same")(conv3)
     # conv5 = Convolution2D(32, 5, 5, activation='relu', border_mode='same')(conv5)
 
     locnet = Flatten()(conv5)
     locnet = Dense(500)(locnet)
-    locnet = Activation('relu')(locnet)
+    locnet = Activation("relu")(locnet)
     locnet = Dense(200)(locnet)
-    locnet = Activation('relu')(locnet)
+    locnet = Activation("relu")(locnet)
     locnet = Dense(100)(locnet)
-    locnet = Activation('relu')(locnet)
+    locnet = Activation("relu")(locnet)
     locnet = Dense(50)(locnet)
-    locnet = Activation('relu')(locnet)
+    locnet = Activation("relu")(locnet)
     weights = get_initial_weights(50)
     locnet = Dense(6, weights=weights)(locnet)
     x = BilinearInterpolation(sampling_size)([inputs, locnet])
 
     up6 = keras.layers.Concatenate(axis=-1)(
-        [Convolution2D(32, 2, 2, activation='relu', border_mode='same')(UpSampling2D(size=(2, 2))(x)), conv2])
-    conv6 = Convolution2D(32, 5, 5, activation='relu', border_mode='same')(up6)
-    conv6 = Convolution2D(32, 5, 5, activation='relu', border_mode='same')(conv6)
+        [
+            Convolution2D(32, 2, 2, activation="relu", border_mode="same")(
+                UpSampling2D(size=(2, 2))(x)
+            ),
+            conv2,
+        ]
+    )
+    conv6 = Convolution2D(32, 5, 5, activation="relu", border_mode="same")(up6)
+    conv6 = Convolution2D(32, 5, 5, activation="relu", border_mode="same")(conv6)
 
     up7 = keras.layers.Concatenate(axis=-1)(
-        [Convolution2D(32, 2, 2, activation='relu', border_mode='same')(UpSampling2D(size=(2, 2))(conv6)), conv1])
-    conv7 = Convolution2D(32, 5, 5, activation='relu', border_mode='same')(up7)
-    conv7 = Convolution2D(32, 5, 5, activation='relu', border_mode='same')(conv7)
+        [
+            Convolution2D(32, 2, 2, activation="relu", border_mode="same")(
+                UpSampling2D(size=(2, 2))(conv6)
+            ),
+            conv1,
+        ]
+    )
+    conv7 = Convolution2D(32, 5, 5, activation="relu", border_mode="same")(up7)
+    conv7 = Convolution2D(32, 5, 5, activation="relu", border_mode="same")(conv7)
 
-    conv10 = Convolution2D(1, 5, 5, activation='linear', border_mode='same')(conv7)
+    conv10 = Convolution2D(1, 5, 5, activation="linear", border_mode="same")(conv7)
 
     model = Model(input=inputs, output=conv10)
 
